bec1187b4fa38c830f535dae42c1ae4fd7e26ba2
AboBrat <nikola.drobnjak999@gmail.com>
2023-08-12 16:34:16 +0200

    update

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,8 +1,8 @@
 def call(String project, String ImageTag, String hubUser){
     sh """
 
-    trivy image ${hubUser}/${project} ${hubUser}/${project}:latest > scan.txt
+    trivy image ${hubUser}/${project}:latest > scan.txt
     cat scan.txt
-    
+
     """
 }
